53c92c222f43cb950abe750e1517e86717216535
Amod Panchal <32383876+amod26@users.noreply.github.com>
2020-04-18 15:12:08 -0400

    Reverse String
    different ways to reverse a string.

---
+++ Reverse String.py
@@ -0,0 +1,15 @@
+s = ["h", "e", "l", "l", "o"]
+
+# Reverse string with different ways to solve
+
+
+def reverseString1(s):
+    return s.reverse()
+
+
+def reverseString2(s):
+    return (s[::-1])
+
+
+print(reverseString1(s))
+print(reverseString2(s))
