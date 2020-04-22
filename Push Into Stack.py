8f7ac4532693ae58308706346f6268454b449968
Amod Panchal <32383876+amod26@users.noreply.github.com>
2020-04-22 11:48:56 -0400

    Push Data into Stack

---
+++ Push Into Stack.py
@@ -0,0 +1,26 @@
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def add(self, dataval):
+        # Use list append method to add element
+        if dataval not in self.stack:
+            self.stack.append(dataval)
+            return True
+        else:
+            return False
+# Use peek to look at the top of the stack
+
+    def peek(self):
+        return self.stack[-1]
+
+
+AStack = Stack()
+AStack.add("Mon")
+AStack.add("Tue")
+AStack.peek()
+print(AStack.peek())
+AStack.add("Wed")
+AStack.add("Thu")
+print(AStack.peek())
