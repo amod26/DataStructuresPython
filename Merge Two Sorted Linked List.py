a408230216f07867349232cd2d8cf416f4d5653e
Amod Panchal <32383876+amod26@users.noreply.github.com>
2020-05-02 16:30:04 -0400

    Updated Merge two sorted list
    Added recursion method

--- Merge Two Sorted Linked List.py
+++ Merge Two Sorted Linked List.py
@@ -30,3 +30,16 @@ class Solution:
 
         head = head.next
         return head
+
+## Recrusion 
+   def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
