43acab93f48e193504d25b8aaff60ae2fb390eba
Amod Panchal <32383876+amod26@users.noreply.github.com>
2020-05-02 16:43:21 -0400

    Updated with Sentinel Node code

--- Remove element from LinkedList.py
+++ Remove element from LinkedList.py
@@ -23,3 +23,20 @@ class Solution:
                 prev = p
             p = p.next
         return head
+
+    
+    #Adding a sentinel Node
+    # Sentinel node is nothing but an empty list which can be initialized at the head or tail of the linkedlist
+    
+     def removeElements(self, head: ListNode, val: int) -> ListNode:
+        sentinel = ListNode(0)
+        sentinel.next = head
+        
+        prev, curr = sentinel, head
+        while curr:
+            if curr.val == val:
+                prev.next = curr.next
+            else:
+                prev = curr
+            curr = curr.next
+        return sentinel.next
